a3be9c1508e5e8a8d826ad414662681d183b571b
mathildehaugum <mathilde_haugum@hotmail.com>
2020-09-17 15:14:14 +0200

    Add Footer component with CSS styling

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./AppStyle.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <a
+        className="footer-link"
+        href="https://www.linkedin.com/in/mathilde-haugum-060935114/"
+      >
+        LinkedIn
+      </a>
+      <a className="footer-link" href="https://github.com/mathildehaugum">
+        Github
+      </a>
+      <p className="copyright">© 2020 Mathilde Haukø Haugum.</p>
+    </div>
+  );
+}
+
+export default Footer;
