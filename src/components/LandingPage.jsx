6ec773e4f2553f17a611f0197a7b879a68f84493
mathildehaugum <mathilde_haugum@hotmail.com>
2020-09-17 15:13:40 +0200

    Add App and LandingPage component

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./AppStyle.css";
+import Footer from "./Footer";
+import NavBarComponent from "./NavBarComponent";
+
+function LandingPage() {
+  return (
+    <div>
+      <NavBarComponent />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default LandingPage;
