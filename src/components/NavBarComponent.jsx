1cd6b94064e0a9cb989ec242aec9e7eb715bd05c
mathildehaugum <mathilde_haugum@hotmail.com>
2020-09-18 20:31:53 +0200

    Add NavBar component

--- src/components/NavBarComponent.jsx
+++ src/components/NavBarComponent.jsx
@@ -1,25 +1,34 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
+import "./index.css";
 
 function NavBarComponent() {
   return (
-    <Navbar bg="transparent" expand="lg">
-      <Navbar.Brand href="#home">Mathilde Haugum</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto px-3">
-          <Nav.Link href="#home" className="px-4">
-            Om meg
-          </Nav.Link>
-          <Nav.Link href="#link" className="px-4">
-            Arbeid
-          </Nav.Link>
-          <Nav.Link href="#link" className="px-4">
-            CV
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <div>
+      <div className="navbar-image">
+        <div>
+          <Navbar bg="transparent" expand="lg" overflow="hidden">
+            <Navbar.Brand href="#home" className="navbar-brand">
+              <span className="navbrand-styling">Mathilde Haukø Haugum</span>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ml-auto px-3">
+                <Nav.Link href="#home" className="px-4">
+                  <span className="navlink-styling">Om meg</span>
+                </Nav.Link>
+                <Nav.Link href="#link1" className="px-4" bg="light">
+                  <span className="navlink-styling">Arbeid</span>
+                </Nav.Link>
+                <Nav.Link href="#link2" className="px-4">
+                  <span className="navlink-styling">CV</span>
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
+      </div>
+    </div>
   );
 }
 
