3ffae34eca6e8b84e91961012a319c17841fe761
mathildehaugum <mathilde_haugum@hotmail.com>
2020-09-18 20:32:16 +0200

    Add middlecontainer component with text

---
+++ src/components/MiddleContainerComponent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+
+function MiddleContainerComponent() {
+  return (
+    <div className="middle-container">
+      <p className="middle-container-text">
+        Hei! Mitt navn er Mathilde, og jeg studerer Datateknologi ved NTNU med
+        spesialisering innenfor Kunstig Intelligens. Jeg brenner for
+        programvareutvikling og elsker å være kreativ og løse problemer.
+      </p>
+    </div>
+  );
+}
+
+export default MiddleContainerComponent;
