107c310170172debb2e1d1f9be1bed5ee9f52f3f
mathildehaugum <mathilde_haugum@hotmail.com>
2020-09-18 20:31:29 +0200

    Update Footer component and add FooterIcon component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,41 @@
 import React from "react";
-import "./AppStyle.css";
+import { Container, Row, Col } from "react-bootstrap";
+import "./index.css";
+import { mailIcon, githubIcon, linkedinIcon } from "../images";
+import FooterIcon from "./FooterIcon";
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
-    <div className="footer">
-      <a
-        className="footer-link"
-        href="https://www.linkedin.com/in/mathilde-haugum-060935114/"
-      >
-        LinkedIn
-      </a>
-      <a className="footer-link" href="https://github.com/mathildehaugum">
-        Github
-      </a>
-      <p className="copyright">© 2020 Mathilde Haukø Haugum.</p>
-    </div>
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col align="center">
+            <FooterIcon
+              icon={mailIcon}
+              name="Mail"
+              link="mailto:mathilde_haugum@hotmail.com"
+            />
+          </Col>
+          <Col align="center">
+            <FooterIcon
+              icon={githubIcon}
+              name="Github"
+              link="https://github.com/mathildehaugum"
+            />
+          </Col>
+          <Col align="center">
+            <FooterIcon
+              icon={linkedinIcon}
+              name="LinkdIn"
+              link="https://www.linkedin.com/in/mathilde-haugum-060935114/"
+            />
+          </Col>
+        </Row>
+      </Container>
+
+      <p className="copyright">© {currentYear} Mathilde Haukø Haugum.</p>
+    </footer>
   );
 }
 
---
+++ src/components/FooterIcon.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./index.css";
+import { Row, Image, Col } from "react-bootstrap";
+
+export default function FooterIcon(props) {
+  return (
+    <div style={{ width: 100 }}>
+      <Row style={{ paddingBottom: 10 }}>
+        <Col>
+          <Image src={props.icon} className="icon-footer" />
+        </Col>
+      </Row>
+      <Row>
+        <Col align="center">
+          <a className="footer-link" href={props.link}>
+            {props.name}
+          </a>
+        </Col>
+      </Row>
+    </div>
+  );
+}
