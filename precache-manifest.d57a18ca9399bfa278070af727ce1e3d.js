17844d908f13eaca151e1388f99e91433564bf01
mathildehaugum <mathilde_haugum@hotmail.com>
2020-09-18 20:48:15 +0200

    Updates

---
+++ precache-manifest.d57a18ca9399bfa278070af727ce1e3d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9dea9f005fa966506c33fb58325e08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1425cb6b49bf1323d613",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "cd6ae5d083392fbd9777",
+    "url": "/static/css/main.eaeece9d.chunk.css"
+  },
+  {
+    "revision": "1425cb6b49bf1323d613",
+    "url": "/static/js/2.90f0b787.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.90f0b787.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd6ae5d083392fbd9777",
+    "url": "/static/js/main.a910be11.chunk.js"
+  },
+  {
+    "revision": "fb8729707cf02e1d905e",
+    "url": "/static/js/runtime-main.d6a4f8b3.js"
+  },
+  {
+    "revision": "806c5d502a2ee5c58014535810c9c1e3",
+    "url": "/static/media/github.806c5d50.svg"
+  },
+  {
+    "revision": "d01704881e3bbba67e4ca72d1bce5101",
+    "url": "/static/media/linkdIn.d0170488.svg"
+  },
+  {
+    "revision": "86af7a47f6b26e9b98bbc8f50e373fcf",
+    "url": "/static/media/mail.86af7a47.svg"
+  },
+  {
+    "revision": "eb1cb7718786349b927924e0fff0279c",
+    "url": "/static/media/profile.eb1cb771.jpeg"
+  }
+]);
