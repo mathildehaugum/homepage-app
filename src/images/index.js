32f2ee2375ab314db5062365be9405fdba160ba3
mathildehaugum <mathilde_haugum@hotmail.com>
2020-09-18 20:30:46 +0200

    Make constants linked to images

---
+++ src/images/index.js
@@ -0,0 +1,4 @@
+export const profile = require("./profile.jpeg");
+export const mailIcon = require("./mail.svg");
+export const githubIcon = require("./github.svg");
+export const linkedinIcon = require("./linkdIn.svg");
