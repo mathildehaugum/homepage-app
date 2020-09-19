9ea8757e721c7896811efbd5e7c192949cef2889
mathildehaugum <mathilde_haugum@hotmail.com>
2020-09-19 16:23:53 +0200

    Add navigation between different pages

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import About from "./components/About";
+import Work from "./components/Work";
+import CvComponent from "./components/CvComponent";
+import Error from "./components/Error";
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route path="/" component={LandingPage} exact />
+            <Route path="/about" component={About} />
+            <Route path="/work" component={Work} />
+            <Route path="/cv" component={CvComponent} />
+            <Route component={Error} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
