054305700f21033eb0aa5f92e4c2664ec5162900
mathildehaugum <mathilde_haugum@hotmail.com>
2020-09-20 20:37:18 +0200

    Add cv component

--- src/components/CvComponent.jsx
+++ src/components/CvComponent.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import NavBar from "./NavBar";
+import { Link } from "react-router-dom";
+import CvPdf from "../assets/cv.pdf";
+import { Row, Col } from "react-bootstrap";
 
 function CvComponent() {
   return (
     <div>
       <NavBar />
-      <p>CV</p>
+      <div className="cv-div">
+        <Row>
+          <h2>Min CV</h2>
+        </Row>
+        <Row>
+          <p>Hvis du ønsker å se min CV, kan du laste den ned her: </p>
+        </Row>
+        <Row>
+          <Col align="center">
+            <Link to={CvPdf} target="_blank" download>
+              <button className="cv-btn">Last ned</button>
+            </Link>
+          </Col>
+        </Row>
+      </div>
+      <div style={{ height: 530 }}></div>
     </div>
   );
 }
