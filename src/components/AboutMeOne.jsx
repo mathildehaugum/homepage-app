72aa003d07c87169adaabbb5c9f33f613b4a4982
mathildehaugum <mathilde_haugum@hotmail.com>
2020-09-23 15:34:06 +0200

    Update text and styling of about-me componentes

--- src/components/AboutMeOne.jsx
+++ src/components/AboutMeOne.jsx
@@ -6,27 +6,32 @@ function AboutMeOne() {
   return (
     <Container className="container-padding">
       <Row>
-        <Col className="col-align" md={12} lg={6}>
-          <div className="about-me-div">
-            <h2>Om meg</h2>
-            <p className>
-              Jeg heter Mathilde, er 24 år og går på fjerde året på
-              Datateknologi ved NTNU. Jeg har valgt spesialisering innenfor
-              Kunstig Intelligens, og blir stadig utfordret med nye og
-              interessante teknologier. Før jeg begynte på Datateknologi
-              studerte jeg fire år på Nanoteknologi ved NTNU. Jeg har alltid
-              vært glad i teknologi og ser frem til å delta i fremtidig
-              utvikling av innovative produkter. Jeg heter Mathilde, er 24 år og
-              går på fjerde året på Datateknologi ved NTNU. Jeg har valgt
-              spesialisering innenfor Kunstig Intelligens, og blir stadig
-              utfordret med nye og interessante teknologier. Før jeg begynte på
-              Datateknologi studerte jeg fire år på Nanoteknologi ved NTNU. Jeg
-              har alltid vært glad i teknologi og ser frem til å delta i
-              fremtidig utvikling av innovative produkter.
-            </p>
-          </div>
+        <div className="div-header-left">
+          <h2>Om meg</h2>
+        </div>
+      </Row>
+      <Row>
+        <Col md={12} lg={6} className="col-padding">
+          <p className="div-paragraph">
+            Jeg heter Mathilde, er 24 år og kommer fra Namsos som er en liten by
+            tre timer nord for Trondheim. I løpet av oppveksten har jeg alltid
+            vært interessert i teknologi og problemløsning, og jeg har hatt en
+            sterk indre motivasjon for å jobbe hardt med skolearbeid. Etter å ha
+            fullført videregående flyttet jeg til Trondheim for å begynne på
+            masterstudiet i Nanoteknologi ved NTNU. Her ble jeg kjent med mange
+            hyggelige medstudenter og lærte mye om fremtidens teknologi.
+            Samtidig hadde jeg mitt første møte med programmering innenfor emnet
+            ITGK og dette ga mersmak. Etterhvert som vi fikk høre om kunstig
+            intelligens, IoT, programvaresikkerhet, maskinlæring og Big Data
+            kjente jeg et sterkt behov for å lære mer om disse områdene. Derfor
+            tok jeg avgjørelsen å bytte over til masterstudiet Datateknologi ved
+            NTNU, og jeg har ikke angret siden. Nå går jeg på mitt fjerde år med
+            spesialisering innenfor kunstig intelligens, og jeg trives svært
+            godt med dette. Jeg gleder meg til arbeidslivet og er spent på alt
+            jeg skal lære i fremtiden.
+          </p>
         </Col>
-        <Col className="col-align" md={12} lg={6}>
+        <Col className="col-align col-padding" md={12} lg={6}>
           <img src={drawing} alt="profile-img" width="420" height="400" />
         </Col>
       </Row>
--- src/components/AboutMeTwo.jsx
+++ src/components/AboutMeTwo.jsx
@@ -4,24 +4,29 @@ import { hiking } from "../images";
 
 function AboutMeTwo() {
   return (
-    <Container className="container-padding, bottom-padding">
+    <Container className="bottom-padding top-padding">
       <Row>
-        <Col className="col-align" md={12} lg={6}>
-          <img src={hiking} alt="profile-img" width="540" height="400" />
+        <Col className="col-align col-padding" md={12} lg={6}>
+          <img src={hiking} alt="profile-img" width="500" height="360" />
         </Col>
-        <Col className="col-align" md={12} lg={6}>
-          <div className="about-me-div">
+        <Col className="col-padding" md={12} lg={6}>
+          <div className="div-header-right">
             <h2>Mine hobbyer</h2>
-            <p className>
-              Jeg bor på Tiller sammen med min samboer og våre to hunder, Milo
-              og Zelda. Ved siden av studiene liker jeg å bruke tiden ut i
-              naturen eller i sofaen med spaken til en spillkonsoll. Jeg bor på
-              Tiller sammen med min samboer og våre to hunder, Milo og Zelda.
-              Ved siden av studiene liker jeg å bruke tiden ut i naturen eller i
-              sofaen med spaken til en spillkonsoll. Jeg bor på Tiller sammen
-              med min samboer og våre to hunder, Milo og Zelda. Ved siden av
-              studiene liker jeg å bruke tiden ut i naturen eller i sofaen med
-              spaken til en spillkonsoll.
+            <p className="div-paragraph">
+              Jeg bor på Tiller sammen med min samboer, Espen, og våre to
+              hunder, Milo og Zelda. Jeg har alltid vært svært glad i dyr og har
+              vokst opp med en familiehund. Derfor var det en stor glede når vi
+              fikk muligheten til å få våre egne hunder, og jeg bruker mye tid
+              på å trene og være med dem. Livet med hund har også gjort at jeg
+              har blitt svært glad i tur og friluftsliv, og i helgene befinner
+              jeg meg ofte på fjellet med en stor sekk som rommer sovepose, telt
+              og primusen. Dersom jeg ikke er ute å farter kan jeg gjerne bruke
+              tid på dataspill, og utviklingen innenfor spillteknologi har vært
+              en bidragsyter til min nysgjerrighet innenfor teknologi. I løpet
+              av den siste tiden har jeg også lært mye om utvikling av
+              nettapplikasjoner gjennom et kurs på udemy, og jeg har brukt mye
+              av tiden ved siden av deltidsjobb og studie på å videreutvikle
+              mine ferdigheter innenfor web utvikling.
             </p>
           </div>
         </Col>
