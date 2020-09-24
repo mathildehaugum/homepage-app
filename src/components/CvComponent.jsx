352325ac4e49751002f9865f8d2e7ea92fc3938b
mathildehaugum <mathilde_haugum@hotmail.com>
2020-09-24 15:15:07 +0200

    Remove fixed sizing of CV component to adapt to mobile display

--- src/components/CvComponent.jsx
+++ src/components/CvComponent.jsx
@@ -2,28 +2,20 @@ import React from "react";
 import NavBar from "./NavBar";
 import { Link } from "react-router-dom";
 import CvPdf from "../assets/cv.pdf";
-import { Row, Col } from "react-bootstrap";
 
 function CvComponent() {
   return (
     <div>
       <NavBar />
-      <div className="cv-div">
-        <Row>
-          <h2>Min CV</h2>
-        </Row>
-        <Row>
-          <p>Hvis du ønsker å se min CV, kan du laste den ned her: </p>
-        </Row>
-        <Row>
-          <Col align="center">
-            <Link to={CvPdf} target="_blank" download>
-              <button className="cv-btn">Last ned</button>
-            </Link>
-          </Col>
-        </Row>
+      <div className="cv-div wrapper">
+        <p>
+          Hvis du ønsker å se min CV, kan du laste den ned{" "}
+          <Link to={CvPdf} target="_blank" download>
+            her
+          </Link>
+          .
+        </p>
       </div>
-      <div style={{ height: 530 }}></div>
     </div>
   );
 }
