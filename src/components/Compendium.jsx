df1214cae33327492042eaa0809f73d0441b6902
mathildehaugum <mathilde_haugum@hotmail.com>
2020-09-24 08:57:09 +0200

    Fix sizing problem with mobile display

--- src/components/Compendium.jsx
+++ src/components/Compendium.jsx
@@ -18,7 +18,7 @@ import {
   osIMG,
 } from "../images";
 import { Link } from "react-router-dom";
-import { Container } from "react-bootstrap";
+import { Container, Col } from "react-bootstrap";
 
 function Compendium() {
   return (
@@ -53,12 +53,16 @@ function Compendium() {
         <h4 className="compendium-time" style={{ color: "#436485" }}>
           Høsten 2018
         </h4>
-        <p className="compendium-desc">
-          I dette emnet lærte vi metoder for å analysere effektiviteten til
-          algoritmer innenfor ordning, søking og sortering av datamengder. Dette
-          var et utfordrende og svært lærerikt fag, og jeg likte at det var
-          fokus på forståelse i stedet for pugging.
-        </p>
+        <Container>
+          <Col className="compendium-desc" sm={12} md={8}>
+            <p>
+              I dette emnet lærte vi metoder for å analysere effektiviteten til
+              algoritmer innenfor ordning, søking og sortering av datamengder.
+              Dette var et utfordrende og svært lærerikt fag, og jeg likte at
+              det var fokus på forståelse i stedet for pugging.
+            </p>
+          </Col>
+        </Container>
         <Link to={algdatPDF} target="_blank" download>
           <button
             className="compendium-btn"
@@ -81,12 +85,16 @@ function Compendium() {
         <h4 className="compendium-time" style={{ color: "#f59292" }}>
           Våren 2020
         </h4>
-        <p className="compendium-desc">
-          Dette er et av de mest spennende emnene jeg har hatt ved NTNU. Vi
-          lærte om sikkerhetens rolle i SDLC, og vi fikk i oppgave å bruke OWASP
-          testguide og Risk Management Framework for å finne utplasserte
-          sårbarheter ved en gitt nettside.
-        </p>
+        <Container>
+          <Col className="compendium-desc" sm={12} md={8}>
+            <p>
+              Dette er et av de mest spennende emnene jeg har hatt ved NTNU. Vi
+              lærte om sikkerhetens rolle i SDLC, og vi fikk i oppgave å bruke
+              OWASP testguide og Risk Management Framework for å finne
+              utplasserte sårbarheter ved en gitt nettside.
+            </p>
+          </Col>
+        </Container>
         <Link to={progsikkPDF} target="_blank" download>
           <button
             className="compendium-btn"
@@ -111,13 +119,17 @@ function Compendium() {
         <h4 className="compendium-time" style={{ color: "#535b9e" }}>
           Høsten 2019
         </h4>
-        <p className="compendium-desc">
-          I dette emnet lærte vi om de grunnleggende prinsippene for bruk av
-          kunstig intelligens for problemløsning, søk, inferens og
-          kunnskapsrepresentasjon. Dette ble mitt første ordentlige møte med
-          kunstig intelligens, og det styrket min motivasjon for å lære mer om
-          dette området.
-        </p>
+        <Container>
+          <Col className="compendium-desc" sm={12} md={8}>
+            <p>
+              I dette emnet lærte vi om de grunnleggende prinsippene for bruk av
+              kunstig intelligens for problemløsning, søk, inferens og
+              kunnskapsrepresentasjon. Dette ble mitt første ordentlige møte med
+              kunstig intelligens, og det styrket min motivasjon for å lære mer
+              om dette området.
+            </p>
+          </Col>
+        </Container>
         <Link to={introaiPDF} target="_blank" download>
           <button
             className="compendium-btn"
@@ -142,13 +154,17 @@ function Compendium() {
         <h4 className="compendium-time" style={{ color: "#ffdeb6" }}>
           Våren 2019
         </h4>
-        <p className="compendium-desc">
-          Dette emnet introduserte konsepter innenfor design, håndtering og
-          konstruksjon av databaser. I øvingene fikk vi i oppgave å lage en
-          treningsdagbok, og det ble mitt første møte med hvordan MySQL kan
-          brukes for å lagre og hente data fra en ekstern server, noe som ga
-          stor mestrinsfølelse når vi fikk det til.
-        </p>
+        <Container>
+          <Col className="compendium-desc" sm={12} md={8}>
+            <p className="compendium-desc">
+              Dette emnet introduserte konsepter innenfor design, håndtering og
+              konstruksjon av databaser. I øvingene fikk vi i oppgave å lage en
+              treningsdagbok, og det ble mitt første møte med hvordan MySQL kan
+              brukes for å lagre og hente data fra en ekstern server, noe som ga
+              stor mestrinsfølelse når vi fikk det til.
+            </p>
+          </Col>
+        </Container>
         <Link to={databasePDF} target="_blank" download>
           <button
             className="compendium-btn"
@@ -171,14 +187,18 @@ function Compendium() {
         <h4 className="compendium-time" style={{ color: "#306844" }}>
           Høsten 2019
         </h4>
-        <p className="compendium-desc">
-          I dette emnet lærte vi om ulike typer informasjonssystemer, hvordan
-          man kan identifisere organisasjoners behov og hvordan man kan fremme
-          suksessfull implementering av endringer. Dette var et spennende emne,
-          for tidligere har vi lært hvordan man kan lage systemene, men nå fikk
-          vi også lære hvordan man kan legge til rette for at systemet faktisk
-          blir brukt av kunden.
-        </p>
+        <Container>
+          <Col className="compendium-desc" sm={12} md={8}>
+            <p className="compendium-desc">
+              I dette emnet lærte vi om ulike typer informasjonssystemer,
+              hvordan man kan identifisere organisasjoners behov og hvordan man
+              kan fremme suksessfull implementering av endringer. Dette var et
+              spennende emne, for tidligere har vi lært hvordan man kan lage
+              systemene, men nå fikk vi også lære hvordan man kan legge til
+              rette for at systemet faktisk blir brukt av kunden.
+            </p>
+          </Col>
+        </Container>
         <Link to={infosysPDF} target="_blank" download>
           <button
             className="compendium-btn"
@@ -201,14 +221,18 @@ function Compendium() {
         <h4 className="compendium-time" style={{ color: "#fcbb9d" }}>
           Våren 2019
         </h4>
-        <p className="compendium-desc">
-          Etter å ha lært en del om generell programmering var det på tide å få
-          mer kunnskap i hvordan man skaper gode brukeroppleveler. I
-          øvingsopplegget fikk vi i oppgave å lage en lavnivå prototype av en
-          mobil applikasjon og gjennomføre brukertesting. Jeg synes det var
-          interessant å lære om denne delen av programvareutvikling og fint å få
-          et større utløp for min kreativitet.
-        </p>
+        <Container>
+          <Col className="compendium-desc" sm={12} md={8}>
+            <p className="compendium-desc">
+              Etter å ha lært en del om generell programmering var det på tide å
+              få mer kunnskap i hvordan man skaper gode brukeroppleveler. I
+              øvingsopplegget fikk vi i oppgave å lage en lavnivå prototype av
+              en mobil applikasjon og gjennomføre brukertesting. Jeg synes det
+              var interessant å lære om denne delen av programvareutvikling og
+              fint å få et større utløp for min kreativitet.
+            </p>
+          </Col>
+        </Container>
         <Link to={mmiPDF} target="_blank" download>
           <button
             className="compendium-btn"
@@ -231,13 +255,17 @@ function Compendium() {
         <h4 className="compendium-time" style={{ color: "#784d9b" }}>
           Våren 2020
         </h4>
-        <p className="compendium-desc">
-          I dette emnet lærte vi om prinsipper innenfor moderne
-          operativsystemer, og hvordan ulike teknikker kan brukes for å
-          effektivt utnytte systemressursene. I tidligere emner har vi ofte hørt
-          om parallell prosessering og fordeler ved dette, så det var gøyalt å
-          endelig lære mer om hvordan det kan implementeres.
-        </p>
+        <Container>
+          <Col className="compendium-desc" sm={12} md={8}>
+            <p className="compendium-desc">
+              I dette emnet lærte vi om prinsipper innenfor moderne
+              operativsystemer, og hvordan ulike teknikker kan brukes for å
+              effektivt utnytte systemressursene. I tidligere emner har vi ofte
+              hørt om parallell prosessering og fordeler ved dette, så det var
+              gøyalt å endelig lære mer om hvordan det kan implementeres.
+            </p>
+          </Col>
+        </Container>
         <Link to={osPDF} target="_blank" download>
           <button
             className="compendium-btn"
